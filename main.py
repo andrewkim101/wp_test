35bb80921afcd0e584b66733f5a60ea52a55c058
kronikus <andrew@houseofkim.com>
2019-08-26 00:14:42 -0400

    unittest

--- main.py
+++ main.py
@@ -3,27 +3,36 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+import unittest
 
-options = Options()
-options.headless = True
+class MainTest(unittest.TestCase):
 
-# WebDriver driver = new RemoteWebDriver(new URL("http://192.168.11.108:4444/wd/hub"), capability)					 
-# driver = webdriver.Remote(
-#    command_executor="http://192.168.11.108:4444/wd/hub",
-#    desired_capabilities={
-#             "browserName": "firefox",
-#             })
-# #print ("Video: " + VIDEO_URL + driver.session_id)
-driver = webdriver.Chrome('/usr/local/bin/chromedriver',chrome_options=options)
-   
-try:
-    driver.implicitly_wait(30)
-    driver.maximize_window() # Note: driver.maximize_window does not work on Linux selenium version v2, instead set window size and window position like driver.set_window_position(0,0) and driver.set_window_size(1920,1080)
-    driver.get("http://www.python.org")
-    assert "Python" in driver.title
-    elem = driver.find_element_by_name("q")
-    elem.send_keys("documentation")
-    elem.send_keys(Keys.RETURN)
-    assert "No results found." not in driver.page_source
-finally:
-    driver.quit()
+	@classmethod
+	def setUp(cls):
+
+		options = Options()
+		options.headless = True
+		cls.driver = webdriver.Chrome('/usr/local/bin/chromedriver',options=options)
+
+	# WebDriver driver = new RemoteWebDriver(new URL("http://192.168.11.108:4444/wd/hub"), capability)					 
+	# driver = webdriver.Remote(
+	#    command_executor="http://192.168.11.108:4444/wd/hub",
+	#    desired_capabilities={
+	#             "browserName": "firefox",
+	#             })
+	# #print ("Video: " + VIDEO_URL + driver.session_id)
+
+	def test_python(self):
+		self.driver.implicitly_wait(30)
+		self.driver.maximize_window() # Note: driver.maximize_window does not work on Linux selenium version v2, instead set window size and window position like driver.set_window_position(0,0) and driver.set_window_size(1920,1080)
+		self.driver.get("http://www.python.org")
+		assert "Python" in driver.title
+		elem = self.driver.find_element_by_name("q")
+		elem.send_keys("documentation")
+		elem.send_keys(Keys.RETURN)
+		assert "No results found." not in self.driver.page_source
+
+	@classmethod
+	def tearDown(cls):
+		cls.driver.quit()
+
