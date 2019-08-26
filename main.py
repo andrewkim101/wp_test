f81670ed5d68b341c4ba6b4b97ed30c0fd4d9028
kronikus <andrew@houseofkim.com>
2019-08-26 00:49:33 -0400

    unittest update

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 import unittest
-
+import platform
 class MainTest(unittest.TestCase):
 
 	@classmethod
@@ -12,7 +12,10 @@ class MainTest(unittest.TestCase):
 
 		options = Options()
 		options.headless = True
-		cls.driver = webdriver.Chrome('/usr/local/bin/chromedriver',options=options)
+		if platform.system() == 'Windows':
+			cls.driver = webdriver.Chrome('./win/chromedriver.exe',options=options)
+		else:
+			cls.driver = webdriver.Chrome('./linux/chromedriver',options=options)
 
 	# WebDriver driver = new RemoteWebDriver(new URL("http://192.168.11.108:4444/wd/hub"), capability)					 
 	# driver = webdriver.Remote(
@@ -25,14 +28,12 @@ class MainTest(unittest.TestCase):
 	def test_python(self):
 		self.driver.implicitly_wait(30)
 		self.driver.maximize_window() # Note: driver.maximize_window does not work on Linux selenium version v2, instead set window size and window position like driver.set_window_position(0,0) and driver.set_window_size(1920,1080)
-		self.driver.get("http://www.python.org")
-		assert "Python" in driver.title
-		elem = self.driver.find_element_by_name("q")
-		elem.send_keys("documentation")
-		elem.send_keys(Keys.RETURN)
-		assert "No results found." not in self.driver.page_source
+		self.driver.get("http://wp.houseofkim.info")
+		
 
 	@classmethod
 	def tearDown(cls):
 		cls.driver.quit()
 
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
