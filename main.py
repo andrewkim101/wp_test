34d2410b684afee4651fdde11a76ce1bbc4a81f4
kronikus <andrew@houseofkim.com>
2019-08-26 23:18:41 -0400

    add contact exist test

--- main.py
+++ main.py
@@ -13,10 +13,12 @@ class MainTest(unittest.TestCase):
 		options = Options()
 		options.headless = True
 		if platform.system() == 'Windows':
-			cls.driver = webdriver.Chrome('./win/chromedriver.exe',options=options)
+			cls.driver = webdriver.Chrome('./Windows/chromedriver.exe',options=options)
 		else:
-			cls.driver = webdriver.Chrome('./linux/chromedriver',options=options)
-
+			cls.driver = webdriver.Chrome('./Linux/chromedriver',options=options)
+		cls.driver.implicitly_wait(30)
+		cls.driver.maximize_window() # Note: driver.maximize_window does not work on Linux selenium version v2, instead set window size and window position like driver.set_window_position(0,0) and driver.set_window_size(1920,1080)
+		cls.driver.get("http://wp.houseofkim.info")
 	# WebDriver driver = new RemoteWebDriver(new URL("http://192.168.11.108:4444/wd/hub"), capability)					 
 	# driver = webdriver.Remote(
 	#    command_executor="http://192.168.11.108:4444/wd/hub",
@@ -25,16 +27,20 @@ class MainTest(unittest.TestCase):
 	#             })
 	# #print ("Video: " + VIDEO_URL + driver.session_id)
 
-	def test_python(self):
-		self.driver.implicitly_wait(30)
-		self.driver.maximize_window() # Note: driver.maximize_window does not work on Linux selenium version v2, instead set window size and window position like driver.set_window_position(0,0) and driver.set_window_size(1920,1080)
-		self.driver.get("http://wp.houseofkim.info")
+	def test_header(self):
 		elem = self.driver.find_element_by_xpath('//*[@id="masthead"]/div[1]/div[2]/div/div/h1/a')
 		self.assertTrue('WP' in elem.text)
 	
+	def test_body(self):
+		elem = self.driver.find_element_by_xpath('//*[@id="panel4"]/div[2]/div/header/h2')
+		self.assertTrue('CONTACT' in elem.text)
+	
+
 	@classmethod
 	def tearDown(cls):
 		cls.driver.quit()
 
 if __name__ == '__main__':
-    unittest.main(verbosity=2)
+    unittest.main(verbosity=1)
+		
+
